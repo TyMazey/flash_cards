33b8d56762c49952b52ff4f935116152ac29c10c
Ty Mazey <tymazey@gmail.com>
2018-12-03 09:13:57 -0700

    adds rubric grade

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ##  Flash Cards
 
 This is the starter repository for the [Flash Cards](http://backend.turing.io/module1/projects/flashcards) project.
+
+My Rubric Grade-
+Functionality - 4
+Ruby Mechanics - 3
+Ruby Conventions - 4
+Testing - 3
+Version Control - 2
---
+++ test/card_generator_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card_generator'
+
+class GeneratorTest < Minitest::Test
+
+  def test_it_exists
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
+    assert_instance_of CardGenerator, cards
+  end
+
+  def test_makes_cards
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
+  end
+
+end
